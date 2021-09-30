1a857d9cdfc6cf945b5be1bb6a6bb4ce542e24f5
Bosohyun <kbh900220@gmail.com>
2021-09-29 22:22:18 -0400

    Initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,12 @@
+//IIFE --- Immediately Invoked Function Expression
+/* app.js, BOHYUN KIM, 301131832, 21.09.28 */
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...")
+    }
+
+    window.addEventListener("load",Start);
+
+})();
